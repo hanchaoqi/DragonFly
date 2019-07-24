12340e05527bee0899c48302e5d3ed4954dde866
hanchaoqi <hanchao_qi@qq.com>
2019-07-24 23:10:03 +0800

    实现执行超时装饰器

---
+++ util.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+#-*-coding:utf-8-*-
+#util.py
+
+import signal
+
+class TimeOutException(Exception):
+    def __init__(self, error="Timeout"):
+        Exception.__init__(self, error)
+
+def set_timeout(time_threshold):
+    def wrapper(func):
+        def handler(signum, frame):
+            raise TimeOutException()
+
+        def deco(*args, **kwargs):
+            signal.signal(signal.SIGALRM, handler)
+            signal.alarm(time_threshold)
+            ret = func(*args, **kwargs)
+            signal.alarm(0)
+            return ret
+
+        return deco
+    return wrapper
